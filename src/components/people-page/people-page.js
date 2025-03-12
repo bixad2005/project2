1e3dad53599bc4af2a1b6339cc3772f6d8dc0589
bixad2005 <34238@iitu.edu.kz>
2025-03-12 09:35:19 +0500

    Step7 Small changes

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -4,10 +4,12 @@ import ItemList from '../item-list/item-list';
 import PersonDetails from '../person-details/person-details';
 import ErrorIndicator from '../error-indicator/error-indicator';
 
+import SwapiService from "../../services/swapi-service";
 import './people-page.css';
 
 export default class PeoplePage extends Component {
 
+    swapiService = new SwapiService()
     state = {
         selectedPerson: null,
         hasError: false
@@ -32,7 +34,9 @@ export default class PeoplePage extends Component {
         return (
             <div className="row mb2">
                 <div className="col-md-6">
-                    <ItemList onItemSelected={this.onPersonSelected} />
+                    <ItemList
+                        onItemSelected = {this.onPersonSelected}
+                        getData={this.swapiService.getAllPeople}/>
                 </div>
                 <div className="col-md-6">
                     <PersonDetails personId={this.state.selectedPerson} />
