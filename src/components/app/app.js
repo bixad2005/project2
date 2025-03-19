ad3f645b43b6b303a612f750a66178d27c1563a7
bixad2005 <34238@iitu.edu.kz>
2025-03-19 12:59:02 +0500

    Step10 upd

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,8 +2,6 @@ import React, {Component} from 'react'
 
 import Header from '../header'
 import RandomPlanet from '../random-planet'
-import ItemList from '../item-list'
-import PersonDetails from '../person-details'
 import SwapiService from '../../services/swapi-service'
 
 import './app.css'
@@ -29,7 +27,7 @@ export default class App extends Component{
         return(
             <div>
                 <Header />
-                <RandomPlanet />
+                <RandomPlanet/>
                 <PeoplePage />
 
             </div>
