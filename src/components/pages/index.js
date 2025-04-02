fe9be4c48c7e0c50434a019d1fad4429cd405d7b
bixad2005 <34238@iitu.edu.kz>
2025-04-02 13:45:19 +0500

    Step22 login, secret pages

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,3 +1,5 @@
 export {default as PeoplePage} from './people-page'
 export {default as PlanetsPage} from './planets-page'
 export {default as StarshipsPage} from './starships-page'
+export {default as LoginPage} from './login-page'
+export {default as SecretPage} from './secret-page'
