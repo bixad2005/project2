1c189f48350c73c8953db93a90907895d78d4ced
bixad2005 <34238@iitu.edu.kz>
2025-04-02 12:36:43 +0500

    Step12 props and prototypes

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import PropTypes from 'prop-types'
 import './item-list.css'
 
 
@@ -28,5 +28,11 @@ const ItemList = (props) => {
 }
 ItemList.defaultProps = {
     onItemSelected: () => {}
+}
+
+ItemList.propTypes = {
+    onItemSelected: PropTypes.func,
+    data: PropTypes.arrayOf(PropTypes.object).isRequired,
+    children: PropTypes.func.isRequired
 }
 export default ItemList
