fe03647c523c49be1a3101e0d68ffa17d2ad161c
bixad2005 <34238@iitu.edu.kz>
2025-04-02 13:29:50 +0500

    Step21 routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,6 +11,9 @@ import ErrorIndicator from "../error-indicator"
 import {SwapiServiceProvider} from '../swapi-service-context'
 import SwapiService from "../../services/swapi-service"
 import { PeoplePage, PlanetsPage, StarshipsPage } from "../pages"
+
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import StarshipDetails from '../sw-components/starship-details';
 export default class App extends Component{
 
     state = {
@@ -30,14 +33,24 @@ export default class App extends Component{
         return(
             <ErrorBoundary>
                 <SwapiServiceProvider value={this.state.swapiService} >
-                    <div className="stardb-app">
+                    <Router>
+                        <div className="stardb-app">
+                            <Header />
+                            <RandomPlanet />
 
-                        <Header/>
-                        <RandomPlanet />
-                        <PeoplePage/>
-                        <PlanetsPage/>
-                        <StarshipsPage/>
-                    </div>
+                            <Route path="/" render={() => <h4>Welcome to StarDB</h4>} exact />
+
+                            <Route path="/people/:id" component={PeoplePage} exact />
+
+                            <Route path="planets" component={PlanetsPage} exact />
+
+                            <Route path="/starships" component={StarshipsPage} exact />
+                            <Route path="/starships/:id" render={({ match }) => {
+                                const {id} = match.params
+                                return <StarshipDetails itemId={id} />
+                            }}/>
+                        </div>
+                    </Router>
                 </SwapiServiceProvider>
             </ErrorBoundary>
         )
